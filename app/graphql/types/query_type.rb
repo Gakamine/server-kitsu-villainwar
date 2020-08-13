5661da235cfaab653d82eebb6fdec675041f75c7
Antoine Joly <jolyantoine@pm.me>
2020-08-13 22:22:46 +0200

    Add checkvote query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,3 +1,5 @@
+require 'rest-client'
+
 module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
@@ -8,6 +10,24 @@ module Types
     end
     def current_round
       Round.where("date = ? ",Time.now)[0]
+    end
+
+    field :check_vote, Integer, null: true do
+      description "Check if user already voted"
+      argument :token, String, required: true
+    end
+    def check_vote(token:)
+      round=Round.where("date = ? ",Time.now)[0]
+      response = RestClient.get("https://kitsu.io/api/edge/users?filter[self]=true", {'Authorization': 'Bearer '+token})
+      if defined?(JSON.parse(response.body)['data'][0]['id'])
+        if Vote.where(rounds_id: round.id, user_id: JSON.parse(response.body)['data'][0]['id']).count >= 1
+          return 1
+        else
+          return 0
+        end
+      else
+        return 0
+      end
     end
 
   end
